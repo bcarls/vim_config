3a9121572d85734f3ba636a9e47537a93e610a49
bcarls <bcarls@gmail.com>
2016-03-11 12:52:30 -0600

    Updating setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Initialize the needed submodules
-git submodule init
-git submodule update
-
 # Link everything up
 ln -s ~/vim_config ~/.vim
 ln -s ~/vim_config/.vimrc ~/.vimrc
+
+# Install plugins
+vim +PlugInstall +qall
+
